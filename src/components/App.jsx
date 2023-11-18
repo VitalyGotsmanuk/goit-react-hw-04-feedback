51cfb129d3f7f2d02a0d1a055fba0d4fdbddb905
VitalyGotsmanuk <v.gotsmanyuk@gmail.com>
2023-11-18 21:57:56 +0200

    0.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,87 +1,83 @@
 import '../index.css';
-import { Component } from 'react';
+//import { Component } from 'react';
+import { useState } from "react";
 
 import { Section } from './Section/Section';
 import { Feedback } from './Feedback/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics'
 
-export class App extends Component {
+export const App = (
+  // for props
+
+) => {
+
+  // const [option, setOption] = useState(() => {
+  
+  //  });
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const option = {good, neutral, bad}
+
+  // state = {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0
+  // };    
+
+  const handleFeedback = (option) => {
     
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };    
 
-  handleOption = (option) => {
-      this.setState((prevState) => {
-      return {
-          [option]: prevState[option] +1,
-      }
-  })};
 
-  // handleGood = () => { 
-  //   //console.log("Good", event)
-  //   this.setState({ good: this.state.good + 1 });
-  //   // console.log("Good", this.state.good)     
-  // };
 
-  // handleNeutral = () => { 
-  //   //console.log("Neutral", event)
-  //   this.setState({ neutral: this.state.neutral + 1 });
-  // };
+      // this.setState((prevState) => {
+      // return {
+      //     [option]: prevState[option] +1,
+      // }
+      // })
+  
+  };
 
-  // handleBad = () => { 
-  //   //console.log("Bad", event)
-  //   this.setState({ bad: this.state.bad + 1 }); 
-  // };
-
-  countTotalFeedback = (state) => {
-      let total = (state.good + state.bad + state.neutral);
+  const countTotalFeedback = () => {
+      let total = (good + bad + neutral);
       //console.log(total)
       return total
   }
 
-  countPositiveFeedbackPercentage = (state) => {
-      let positive = state.good / (state.good + state.bad + state.neutral) * 100;
+  const countPositiveFeedbackPercentage = () => {
+      let positive = good / (good + bad + neutral) * 100;
       return Math.round(positive);
   }
 
-  render() {
-
-    const isFeedback = this.countTotalFeedback(this.state);
-
-    return (
-      <>
-        <h1>2-nd feedback HW! 👍</h1>
-
-        <Section title="Please leave feedback"> 
-            <Feedback
-            // handleGood={this.handleGood}
-            // handleNeutral={this.handleNeutral}
-            // handleBad={this.handleBad}
-
-          option={Object.keys(this.state)}
-          onLeaveFeedback={this.handleOption}
-        />  
-        </Section>
+  const isFeedback = countTotalFeedback();
+  return (
+    <>
+    <h1>4-th feedback HW! 👍</h1>
+    <Section title="Please leave feedback"> 
+      <Feedback
+        option={Object.keys(option)}
+        onLeaveFeedback={Feedback}
+      />  
+      </Section>
         
-        {isFeedback === 0 ? <Section
-          title="There is no feedback" /> : 
+      {isFeedback === 0 ? <Section
+        title="There is no feedback" /> : 
 
-        <Section title = "Statistics">  
-          <Statistics
-            state={this.state }
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
+      <Section title = "Statistics">  
+        <Statistics
+          //state={this.state }
+          //option = {optionData}
+          good={good}
+          neutral={neutral}
+          bad={bad}
 
-            countTotalFeedback={this.countTotalFeedback}
-            countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage}
-            />
-          </Section>
+          countTotalFeedback={countTotalFeedback}
+          countPositiveFeedbackPercentage={countPositiveFeedbackPercentage}
+        />
+        </Section>
         }
-      </>
-    );
-  };
-}
+    </>
+  );
+};
